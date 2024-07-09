6b3c8c029842c5dd29b96603363c2b0b31129550
MarcoBolo001 <m.bologna2@studenti.unipi.it>
2024-07-09 15:13:51 +0000

    letter frequency

--- letterfrequency/src/main/java/it/unipi/hadoop/LetterFrequency.java
+++ letterfrequency/src/main/java/it/unipi/hadoop/LetterFrequency.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -11,34 +11,46 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
-public class WordCount {
- public static class TokenizerMapper
- extends Mapper<Object, Text, Text, IntWritable>{
- private final static IntWritable one = new IntWritable(1);
- private Text word = new Text();
- public void map(Object key, Text value, Context context
- ) throws IOException, InterruptedException {
- StringTokenizer itr = new StringTokenizer(value.toString());
- while (itr.hasMoreTokens()) {
- word.set(itr.nextToken());
- context.write(word, one);
+
+public class LetterFrequency {
+
+ public static class LetterFrequencyMapper extends Mapper<Object, Text, Text, LongWritable>{
+
+ private final static LongWritable one = new LongWritable(1);
+
+ private Text letter = new Text();
+
+ public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+ String data = value.toString().toLowerCase();
+for(char c : data.toCharArray()) {
+
+    if (Character.isLetter(c)) {
+        letter.set(Character.toString(c));
+        context.write(letter, one);
+    }
  }
  }
  }
- public static class IntSumReducer
- extends Reducer<Text,IntWritable,Text,IntWritable> {
- private IntWritable result = new IntWritable();
- public void reduce(Text key, Iterable<IntWritable> values,
- Context context
- ) throws IOException, InterruptedException {
- int sum = 0;
- for (IntWritable val : values) {
+
+ public static class LetterFrequencyReducer extends Reducer<Text,LongWritable,Text,LongWritable> {
+
+ private LongWritable result = new LongWritable();
+
+ public void reduce(Text key, Iterable<LongWritable> values,Context context) throws IOException, InterruptedException {
+
+ long sum = 0;
+
+ for (LongWritable val : values) {
  sum += val.get();
  }
+
  result.set(sum);
+
  context.write(key, result);
  }
  }
+
  public static void main(String[] args) throws Exception {
  Configuration conf = new Configuration();
  String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
@@ -46,18 +58,19 @@ public class WordCount {
  System.err.println("Usage: wordcount <in> [<in>...] <out>");
  System.exit(2);
  }
- Job job = Job.getInstance(conf, "word count");
- job.setJarByClass(WordCount.class);
- job.setMapperClass(TokenizerMapper.class);
- job.setCombinerClass(IntSumReducer.class);
- job.setReducerClass(IntSumReducer.class);
+ Job job = Job.getInstance(conf, "letter frequency count");
+ job.setJarByClass(LetterFrequency.class);
+ job.setMapperClass(LetterFrequencyMapper.class);
+ job.setCombinerClass(LetterFrequencyReducer.class);
+ job.setReducerClass(LetterFrequencyReducer.class);
  job.setOutputKeyClass(Text.class);
- job.setOutputValueClass(IntWritable.class);
+ job.setOutputValueClass(LongWritable.class);
+
  for (int i = 0; i < otherArgs.length - 1; ++i) {
  FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
  }
- FileOutputFormat.setOutputPath(job,
- new Path(otherArgs[otherArgs.length - 1]));
+ FileOutputFormat.setOutputPath(job,new Path(otherArgs[otherArgs.length - 1]));
+ 
  System.exit(job.waitForCompletion(true) ? 0 : 1);
  }
 }
